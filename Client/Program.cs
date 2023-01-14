fa34e2c664bddbf98c99b01f1b1dc6ba2e07acb7
Ahme_if16 <Ahme_if16@STHQ012B-08>
2023-01-14 16:56:55 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var ipAdress = IPAddress.Parse("10.2.11.43");
+            var port = 27001;
+
+            var ep = new IPEndPoint(ipAdress, port);
+
+            try
+            {
+                socket.Connect(ep);
+                if (socket.Connected)
+                {
+                    Console.WriteLine("Connected to server . . .");
+                    while (true)
+                    {
+                        var msg = Console.ReadLine();
+                        var bytes = Encoding.UTF8.GetBytes(msg);
+                        socket.Send(bytes);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cannot connect to server . . .");
+            }
+        }
+    }
+}
